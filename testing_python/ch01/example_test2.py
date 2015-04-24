910ecac9c0dd0eab4e671809f0ea0c21d43fd36d
Andrew <ouyangchongwu@gmail.com>
2015-04-25 06:48:17 +0800

    Add  example_test*

---
+++ testing_python/ch01/example_test2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+Demo simple unittest!
+Copyright 2015.04.25 Rongzhong.Xu
+http://automationtesting.sinaapp.com
+"""
+
+
+import unittest
+
+
+class TestDemo(unittest.TestCase):
+    """
+    assertRaises demo
+    """
+    def test_assert_raises(self):
+        with self.assertRaises(AttributeError):
+            [].get
+
+if __name__ == '__main__':
+    unittest.main()
