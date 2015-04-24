4780868795e96f668ce8cce8edb45ed20a1bd60a
ouyangchongwu@gmail.com <ouyangchongwu@gmail.com>
2015-04-24 11:44:08 +0800

    add ch01_calculate_test_err.py

---
+++ testing_python/ch01_calculate_test_err.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+Demo simple unittest!
+Copyright 2015.04.17 Rongzhong.Xu
+http://automationtesting.sinaapp.com
+"""
+
+
+import unittest
+from ch01_calculate import Calculate
+
+
+class TestCalculate(unittest.TestCase):
+    """
+    Calculator unittest
+    """
+    def setUp(self):
+        self.calc = Calculate()
+
+    def test_add_method_returns_correct_result(self):
+        self.assertEqual(4, self.calc.add(2, 3))
+
+
+if __name__ == '__main__':
+    unittest.main()
